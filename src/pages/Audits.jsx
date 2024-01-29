5bf9d4e7bfadb68c8e19da15d19e85aefc766cf1
maximolds <emmanuel.maximo03@gmail.com>
2024-01-30 07:13:44 +0800

    fixed audtis

--- src/pages/Audits.jsx
+++ src/pages/Audits.jsx
@@ -5,6 +5,9 @@ import { useStateContext } from '../contexts/ContextProvider';
 import { Header } from '../components';
 
 const Audits = () => {
+
+  const { currentColor, activeMenu, setActiveMenu } = useStateContext();
+
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 text-slate-950 dark:text-white'>
       <Header
@@ -15,7 +18,7 @@ const Audits = () => {
           color: 'white'
         }}
         category='App'
-        title="Documentation"
+        title="Audits"
       />
       <div className='m-2 md:m-10 mt-2 p-2 md:p-10 bg-white
   rounded-3xl flex-1 md:flex shadow-xl justify-between flex-col flex-wrap'>
@@ -23,40 +26,42 @@ const Audits = () => {
           ${activeMenu ? 'justify-center' : ' justify-center md:w[40%] items-center'}`}>
           {/* Erase eventsettings and
         selectedDate when syncing with a database */}
+          <div className='flex flex-col'>
+            <div className=''>
+              <iframe
+                title="Crane 1"
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRVyxbt16Kb9jL8zaD8mu433XUmc05c_akohY_KSUheSih4tGNe4EfES2oqIsrnm0ZZ1poNjBYay0Yb/pubhtml?widget=true&amp;headers=false"
+                width="100%"
+                height="500px"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </div>
 
-          <div>
-            <iframe
-              title="Crane 1"
-              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRVyxbt16Kb9jL8zaD8mu433XUmc05c_akohY_KSUheSih4tGNe4EfES2oqIsrnm0ZZ1poNjBYay0Yb/pubhtml?widget=true&amp;headers=false"
-              width="100%"
-              height="500px"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
+
+            <div className='mt-2'>
+              <iframe
+                title="Crane 2"
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRYTVsHK-lSOqkBDzmdO0HCrBAcw-V_hRKl3Q4gUVOyhOwx-6AR0P045Zaj-2BVN_aq7k1x081v_ng0/pubhtml?widget=true&amp;headers=false"
+                width="100%"
+                height="500px"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </div>
+
+            <div>
+              <iframe
+                title="Crane 3"
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSVjuH_vxMmiGOpt7rv12NH0VHGDv2d2oXjxnfZuG9Yt6OyqsmbrjbAlVu78Z3GSSnlFtBSVBydGt73/pubhtml?widget=true&amp;headers=false"
+                width="100%"
+                height="500px"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </div>
           </div>
 
-
-          <div>
-            <iframe
-              title="Crane 2"
-              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRYTVsHK-lSOqkBDzmdO0HCrBAcw-V_hRKl3Q4gUVOyhOwx-6AR0P045Zaj-2BVN_aq7k1x081v_ng0/pubhtml?widget=true&amp;headers=false"
-              width="100%"
-              height="500px"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-          </div>
-
-          <div>
-            <iframe
-              title="Crane 3"
-              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSVjuH_vxMmiGOpt7rv12NH0VHGDv2d2oXjxnfZuG9Yt6OyqsmbrjbAlVu78Z3GSSnlFtBSVBydGt73/pubhtml?widget=true&amp;headers=false"
-              width="100%"
-              height="500px"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-          </div>
 
         </div>
       </div>
