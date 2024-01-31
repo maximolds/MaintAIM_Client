83bf893306c6fd1b118aa3da400ccd13b353fb93
maximolds <emmanuel.maximo03@gmail.com>
2024-01-31 22:19:52 +0800

    updated buttons

--- src/pages/QRScanner.jsx
+++ src/pages/QRScanner.jsx
@@ -11,8 +11,8 @@ function QRScanner() {
   useEffect(() => {
     const scanner = new Html5QrcodeScanner('reader', {
       qrbox: {
-        width: 250,
-        height: 250,
+        width: 400,
+        height: 400,
       },
       fps: 5,
     })
